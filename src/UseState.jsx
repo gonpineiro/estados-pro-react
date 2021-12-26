7c0cb2904f280bcce8aee602eaf30b487daf0ba1
Gonzalo <gonzalo@estadodigital.com>
2021-12-26 14:04:04 -0300

    useEffect en funcion component

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 function UseState({ name }) {
     const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+    React.useEffect(() => {
+        console.log('Empezando efecto');
+
+        if (loading) {
+            setTimeout(() => {
+                console.log('Haciendo la validacion');
+
+                setLoading(false);
+
+                console.log('Terminando la validacion');
+            }, 3000);
+        }
+
+        console.log('Terminando efecto');
+    }, [loading]);
 
     return (
         <div>
@@ -10,8 +27,10 @@ function UseState({ name }) {
 
             {error && <p>Error: el codigo es incorrecto</p>}
 
+            {loading && <p>Cargando</p>}
+
             <input type="text" placeholder="Codigo de seguridad" />
-            <button onClick={() => setError(!error)}>Comprobar</button>
+            <button onClick={() => setLoading(true)}>Comprobar</button>
         </div>
     );
 }
