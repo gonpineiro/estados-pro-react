8613092078ca42a04c6b764e76fa8bfcdbb03f56
Gonzalo <gonzalo@estadodigital.com>
2021-12-26 17:53:20 -0300

    Estados imperativos con useState

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -7,9 +7,10 @@ function UseState({ name }) {
         value: '',
         error: false,
         loading: false,
+        deleted: false,
+        confirmed: false,
     });
 
-    console.log(state);
     React.useEffect(() => {
         if (state.loading) {
             setTimeout(() => {
@@ -24,30 +25,66 @@ function UseState({ name }) {
                         ...state,
                         loading: false,
                         error: false,
+                        confirmed: true,
                     });
                 }
             }, 3000);
         }
     }, [state.loading]);
 
-    return (
-        <div>
-            <h2>Elminar {name}</h2>
-            <p>Escribe el codigo de seguridad</p>
+    if (!state.deleted && !state.confirmed) {
+        return (
+            <div>
+                <h2>Elminar {name}</h2>
+                <p>Escribe el codigo de seguridad</p>
 
-            {state.error && !state.loading && <p>Error: el codigo es incorrecto</p>}
+                {state.error && !state.loading && <p>Error: el codigo es incorrecto</p>}
 
-            {state.loading && <p>Cargando</p>}
+                {state.loading && <p>Cargando</p>}
 
-            <input
-                type="text"
-                placeholder="Codigo de seguridad"
-                value={state.value}
-                onChange={({ target: { value } }) => setState({ ...state, value: value })}
-            />
-            <button onClick={() => setState({ ...state, loading: true })}>Comprobar</button>
-        </div>
-    );
+                <input
+                    type="text"
+                    placeholder="Codigo de seguridad"
+                    value={state.value}
+                    onChange={({ target: { value } }) => setState({ ...state, value: value })}
+                />
+                <button onClick={() => setState({ ...state, loading: true })}>Comprobar</button>
+            </div>
+        );
+    } else if (state.confirmed && !state.deleted) {
+        return (
+            <>
+                <p>Pedimos confirmación</p>
+                <button
+                    onClick={() => {
+                        setState({ ...state, deleted: true });
+                    }}
+                >
+                    Si, eliminar
+                </button>
+                <button
+                    onClick={() => {
+                        setState({ ...state, confirmed: false, value: '' });
+                    }}
+                >
+                    No, Me arrepiento
+                </button>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <p>Estado Elimiado</p>
+                <button
+                    onClick={() => {
+                        setState({ ...state, confirmed: false, deleted: false, value: '' });
+                    }}
+                >
+                    Recuperar Estado
+                </button>
+            </>
+        );
+    }
 }
 
 export { UseState };
