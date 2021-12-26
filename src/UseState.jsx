e7584512372810702ff394957c0b9ea1713345e0
Gonzalo <gonzalo@estadodigital.com>
2021-12-26 17:28:34 -0300

    Estados compuestos con useState

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -3,33 +3,49 @@ import React from 'react';
 const SECURITY_CODE = 'paradigma';
 
 function UseState({ name }) {
-    const [value, setValue] = React.useState('');
-    const [error, setError] = React.useState(false);
-    const [loading, setLoading] = React.useState(false);
+    const [state, setState] = React.useState({
+        value: '',
+        error: false,
+        loading: false,
+    });
 
+    console.log(state);
     React.useEffect(() => {
-        if (loading) {
-            setError(false);
+        if (state.loading) {
             setTimeout(() => {
-                if (value !== SECURITY_CODE) {
-                    setError(true);
+                if (state.value !== SECURITY_CODE) {
+                    setState({
+                        ...state,
+                        loading: false,
+                        error: true,
+                    });
+                } else {
+                    setState({
+                        ...state,
+                        loading: false,
+                        error: false,
+                    });
                 }
-                setLoading(false);
             }, 3000);
         }
-    }, [loading]);
+    }, [state.loading]);
 
     return (
         <div>
             <h2>Elminar {name}</h2>
             <p>Escribe el codigo de seguridad</p>
 
-            {error && <p>Error: el codigo es incorrecto</p>}
+            {state.error && !state.loading && <p>Error: el codigo es incorrecto</p>}
 
-            {loading && <p>Cargando</p>}
+            {state.loading && <p>Cargando</p>}
 
-            <input type="text" placeholder="Codigo de seguridad" value={value} onChange={({ target: { value } }) => setValue(value)} />
-            <button onClick={() => setLoading(true)}>Comprobar</button>
+            <input
+                type="text"
+                placeholder="Codigo de seguridad"
+                value={state.value}
+                onChange={({ target: { value } }) => setState({ ...state, value: value })}
+            />
+            <button onClick={() => setState({ ...state, loading: true })}>Comprobar</button>
         </div>
     );
 }
